855dfed01f656432c9a86e6b28529f4312975efd
nikolapeevski <Jeratull@hotmail.com>
2018-11-20 00:46:48 +0100

    (type:refactor) Add FIFO LIFO methods

--- algorithms/worklist.py
+++ algorithms/worklist.py
@@ -9,21 +9,60 @@ class Worklist(ABC):
     def __init__(self):
         pass
 
+    def __init__(self, _worklist):
+        self.worklist = _worklist
+
     def isEmpty(self):
+        """
+        Slightly modififed from simply empty which returns an empty list
+        :return:
+        """
         return len(self.worklist) == 0
 
-    def insert(self, constraint):
+    def insertFIFO(self, constraint):
         self.worklist.append(constraint)
 
         return self.worklist
 
-    def extractFIFO(self, constraint):
-        # TODO Implement this
-        pass
+    def insertLIFO(self, constraint):
+        self.worklist.insert(0, constraint)
+
+        return self.worklist
+
+    def extractFIFO(self):
+        """
+        Using First in first out principle
+        :return:
+        """
+        if self.isEmpty():
+            return []
+
+        condition = self.worklist.pop(0)
+
+        # Remove occurences
+        for i in range(len(self.worklist)):
+            if condition == self.worklist[i]:
+                self.worklist.pop(i)
+
+        return condition, self.worklist
 
     def extractLIFO(self, constraint):
-        # TODO Implement this
-        pass
+        """
+        Using Last in first out principle
+        :param constraint:
+        :return:
+        """
+        if self.isEmpty():
+            return []
+
+        condition = self.worklist.pop()
+
+        # Remove occurences
+        for i in range(len(self.worklist)):
+            if condition == self.worklist[i]:
+                self.worklist.pop(i)
+
+        return condition, self.worklist
 
     def iterate(self, list, analysis, ):
         # TODO Implement this
