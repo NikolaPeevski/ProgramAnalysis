c797b3bcd7130ad465eb679ff5a6ced8a8917596
Dadsgoosen <2803ib@gmail.com>
2018-11-24 12:50:31 +0100

    Implemented program iteration and abstract analyse method

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -1,7 +1,37 @@
+from abc import ABC, abstractmethod
+
 from microCTypes.Program import Program
 
 
-class Analysis:
-    # TODO Implement this
+class Analysis(ABC):
+
     def __init__(self, program: Program):
+        """
+        Base class for all program analysis
+        Contains the abstract analyse method that performs
+        the actual analysis step for the program
+        :param program: The program to analyse
+        """
         self.program = program
+
+        self.contraints = []
+
+    def iterate(self, iteration):
+        """
+        Iterate through the program parts
+        For each iteration the analyse method is called
+        :param iteration: The iteration step
+        """
+        if type(iteration) == list:
+            for i in iteration:
+                self.iterate(i)
+        else:
+            self.analyse(iteration)
+
+    @abstractmethod
+    def analyse(self, step):
+        """
+        Perform the analysis and computes the constraints for the step at the current step
+        :param step: The step to analyse
+        """
+        pass
